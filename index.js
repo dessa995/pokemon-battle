24a7bc9df163cd55c0484fe25df18166e261805a
dessa995 <dessa995@outlook.com>
2022-10-06 18:51:38 +0200

    Finished pokemon battle app

--- index.js
+++ index.js
@@ -1,288 +1,377 @@
 let charizardImg = new Image(300, 300);
-charizardImg.src = 'assets/images/Charizard-removebg-preview.png'
+charizardImg.src = "assets/images/Charizard-removebg-preview.png";
 charizardImg.width = 300;
 charizardImg.height = 300;
 let raichuImg = new Image(300, 300);
-raichuImg.src = 'assets/images/raichu-removebg-preview.png'
+raichuImg.src = "assets/images/raichu-removebg-preview.png";
 raichuImg.width = 300;
 raichuImg.height = 300;
 let blastoiseImg = new Image(300, 300);
-blastoiseImg.src = 'assets/images/pokemon-blastoise-nicknames-removebg-preview-removebg-preview.png'
+blastoiseImg.src =
+  "assets/images/pokemon-blastoise-nicknames-removebg-preview-removebg-preview.png";
 blastoiseImg.width = 300;
 blastoiseImg.height = 300;
 let charmeleonImg = new Image(20, 20);
-charmeleonImg.src = 'assets/images/Charmeleon.png'
+charmeleonImg.src = "assets/images/Charmeleon.png";
 charmeleonImg.width = 20;
 charmeleonImg.height = 20;
 let pikachuImg = new Image(250, 250);
-pikachuImg.src = 'assets/images/pikachu-removebg-preview.png'
+pikachuImg.src = "assets/images/pikachu-removebg-preview.png";
 pikachuImg.width = 250;
 pikachuImg.height = 250;
 let wartortleImg = new Image(250, 250);
-wartortleImg.src = 'assets/images/wartortle-removebg-preview.png'
+wartortleImg.src = "assets/images/wartortle-removebg-preview.png";
 wartortleImg.width = 250;
 wartortleImg.height = 250;
 
+let choosenPokemon = {
+  name: "Choosen Pokemon",
+};
+let choosenOpponent = {
+  name: "Choosen Opponent",
+};
+
 let pokemoni = [
-
   {
-      img: charizardImg,
-      name: 'Charizard',
-      type: 'Fire',
-      skills: ['Fire breath', 'Tail swipe', 'Block'],
-      stats: {
-          Attack: 10,
-          deffence: 6,
-          speed: 5,
-
-      },
-      choosen: false,
+    img: charizardImg,
+    name: "Charizard",
+    type: "Fire",
+    skills: ["Fire breath", "Tail swipe", "Block"],
+    stats: {
+      Attack: 10,
+      Deffence: 6,
+      Speed: 5,
+    },
+    choosen: false,
   },
 
   {
-      img: raichuImg,
-      name: 'Raichu',
-      type: 'Electric',
-      skills: ['Thunder spear', 'zig-zag jump', 'Dodge'],
-      stats: {
-          Attack: 6,
-          deffence: 6,
-          speed: 8
-      },
-      choosen: false,
+    img: raichuImg,
+    name: "Raichu",
+    type: "Electric",
+    skills: ["Thunder spear", "zig-zag jump", "Dodge"],
+    stats: {
+      Attack: 6,
+      Deffence: 6,
+      Speed: 8,
+    },
+    choosen: false,
   },
 
   {
-      img: blastoiseImg,
-      name: 'Blastoise',
-      type: 'Water',
-      skills: ['Water cannon', 'spinning shell', 'Block'],
-      stats: {
-          Attack: 6,
-          deffence: 10,
-          speed: 4,
-      },
-      choosen: false,
-  },
-
-  { 
-      img: charmeleonImg,
-      name: 'Charmeleon',
-      type: 'Fire',
-      skills: ['Fire ball', 'dodge'],
-      stats: {
-          Attack: 4,
-          deffence: 4,
-          speed: 6,
-      },
-      choosen: false,
+    img: blastoiseImg,
+    name: "Blastoise",
+    type: "Water",
+    skills: ["Water cannon", "spinning shell", "Block"],
+    stats: {
+      Attack: 6,
+      Deffence: 10,
+      Speed: 4,
+    },
+    choosen: false,
   },
 
   {
-      img: pikachuImg,
-      name: 'Pikachu',
-      type: 'Electric',
-      skills: ['Small zap', 'dodge'],
-      stats: {
-          Attack: 4,
-          deffence: 3,
-          speed: 10,
-      },
-      choosen: false,
+    img: charmeleonImg,
+    name: "Charmeleon",
+    type: "Fire",
+    skills: ["Fire ball", "dodge"],
+    stats: {
+      Attack: 4,
+      Deffence: 4,
+      Speed: 6,
+    },
+    choosen: false,
   },
 
   {
-      img: wartortleImg,
-      name: 'Wartortle',
-      type: 'Water',
-      skills: ['Water breath', 'block'],
-      stats: {
-          Attack: 3,
-          deffence: 6,
-          speed: 5,
-      },
-      choosen: false,
-  }
-]
+    img: pikachuImg,
+    name: "Pikachu",
+    type: "Electric",
+    skills: ["Small zap", "dodge"],
+    stats: {
+      Attack: 4,
+      Deffence: 3,
+      Speed: 10,
+    },
+    choosen: false,
+  },
 
+  {
+    img: wartortleImg,
+    name: "Wartortle",
+    type: "Water",
+    skills: ["Water breath", "block"],
+    stats: {
+      Attack: 3,
+      Deffence: 6,
+      Speed: 5,
+    },
+    choosen: false,
+  },
+];
 
+const divPokemoni = document.querySelector(".pokemoni");
 
+const buttonPokretac = document.createElement("button");
+buttonPokretac.className = "pokretac";
+buttonPokretac.textContent = "Izaberi svog pokemona";
 
-const divPokemoni = document.querySelector('.pokemoni')
+divPokemoni.append(buttonPokretac);
 
-const buttonPokretac = document.createElement('button')
-buttonPokretac.className = 'pokretac'
-buttonPokretac.textContent = 'Izaberi svog pokemona'
+const fightButton = document.createElement("button");
+fightButton.className = "fight-dugme";
+fightButton.textContent = "FIGHT!";
 
-divPokemoni.append(buttonPokretac)
+divPokemoni.append(fightButton);
 
-const fightButton = document.createElement('button')
-fightButton.className = 'fight-dugme'
-fightButton.textContent = 'FIGHT!'
-
-divPokemoni.append(fightButton)
-
-buttonPokretac.addEventListener('click',(event) =>{
- 
+buttonPokretac.addEventListener("click", (event) => {
   event.preventDefault();
-  
 
+  pokemoni.forEach((pokemon) => {
+    let divPrvi = document.createElement("div");
+    divPrvi.className = "prvi";
 
-  pokemoni.forEach(pokemon => {
+    let button1 = document.createElement("button");
+    button1.className = "buttonPrvi";
+    button1.textContent = `${pokemon.name}`;
 
+    divPrvi.appendChild(button1);
+    divPokemoni.append(divPrvi);
 
-      let divPrvi = document.createElement('div')
-      divPrvi.className = 'prvi'
-  
-      let button1 = document.createElement('button')
-      button1.className = 'buttonPrvi'
-      button1.textContent = `${pokemon.name}`
-  
-      divPrvi.appendChild(button1)
-      divPokemoni.append(divPrvi)
+    button1.addEventListener("click", (event) => {
+      button1.textContent = "";
 
-     
-       
-     
+      let divPok = document.createElement("div");
+      divPok.className = "divPok";
 
-      button1.addEventListener('click', (event) => {
-          button1.textContent = ''
-         
-          let divPok = document.createElement('div')
-          divPok.className = 'divPok'
+      let divPar = document.createElement("p");
+      divPar.className = "divPar";
 
-          let divPar = document.createElement('p')
-          divPar.className = 'divPar'
-       
-          divPar.textContent = `Name: ${pokemon.name}.
+      divPar.textContent = `Name: ${pokemon.name}.
           Type: ${pokemon.type}. 
           Skills: ${pokemon.skills}. 
           stats - Attack: ${pokemon.stats.Attack}, 
-          deffence: ${pokemon.stats.deffence}, 
-          speed: ${pokemon.stats.speed}`
-         
-          let buttonIzaberi = document.createElement('button')
-          buttonIzaberi.textContent = 'Choose me!'
-          buttonIzaberi.className = 'izaberi-button'
+          Deffence: ${pokemon.stats.Deffence}, 
+          Speed: ${pokemon.stats.Speed}`;
 
-          button1.append(divPar,buttonIzaberi)
-         
-          divPrvi.append(divPok)
-          
+      let buttonIzaberi = document.createElement("button");
+      buttonIzaberi.textContent = "Choose me!";
+      buttonIzaberi.className = "izaberi-button";
 
-          buttonIzaberi.addEventListener('click', ()=>{
+      button1.append(divPar, buttonIzaberi);
 
+      divPrvi.append(divPok);
 
-            let pokemonImgDiv = document.createElement('div');
-            pokemonImgDiv.className = 'pokemon-img-div';
-            let choosenPokemon = document.createElement('img');
-            choosenPokemon.setAttribute('src', pokemon.img.src);
-            choosenPokemon.setAttribute('alt', pokemon.name);
-            choosenPokemon.className = 'pokemon-img'
+      buttonIzaberi.addEventListener("click", () => {
+        let allDivs = document.querySelectorAll(".prvi");
 
-            pokemonImgDiv.appendChild(choosenPokemon)
-            divPokemoni.append(pokemonImgDiv);            
-          }); 
+        allDivs.forEach((div) => {
+          div.remove();
+        });
 
-          buttonIzaberi.addEventListener('click', ()=>{
-            divPrvi.remove();
-          });
+        let pokemonImgDiv = document.createElement("div");
+        pokemonImgDiv.className = "pokemon-img-div";
+        let choosenPokemon = document.createElement("img");
+        choosenPokemon.setAttribute("src", pokemon.img.src);
+        choosenPokemon.setAttribute("alt", pokemon.name);
+        choosenPokemon.className = "pokemon-img";
 
-          buttonIzaberi.addEventListener('click', ()=>{
-            pokemon.choosen = true;
-          });
-          
-      })
-  })
-})
+        pokemonImgDiv.appendChild(choosenPokemon);
+        divPokemoni.append(pokemonImgDiv);
+      });
 
+      buttonIzaberi.addEventListener("click", () => {
+        divPrvi.remove();
+      });
 
-buttonPokretac.addEventListener('click',()=>{
-  buttonPokretac.remove()
-})
+      buttonIzaberi.addEventListener("click", () => {
+        pokemon.choosen = true;
+        Object.assign(choosenPokemon, { pokemon });
+      });
+    });
+  });
+});
 
+buttonPokretac.addEventListener("click", () => {
+  buttonPokretac.remove();
+});
 
+const divPokemoni2 = document.querySelector(".protivnik");
 
+const buttonProtivnik = document.createElement("button");
+buttonProtivnik.className = "button-protivnik";
+buttonProtivnik.textContent = "Izaberi svog pokemona";
 
+divPokemoni2.append(buttonProtivnik);
 
-const divPokemoni2 = document.querySelector('.protivnik')
+buttonProtivnik.addEventListener("click", () => {
+  pokemoni.forEach((pokemon) => {
+    let divDrugi = document.createElement("div");
+    divDrugi.className = "drugi";
 
-const buttonProtivnik = document.createElement('button')
-buttonProtivnik.className = 'button-protivnik'
-buttonProtivnik.textContent = 'Izaberi svog pokemona'
+    let button2 = document.createElement("button");
+    button2.className = "buttonDrugi";
+    button2.textContent = `${pokemon.name}`;
 
-divPokemoni2.append(buttonProtivnik)
+    divDrugi.appendChild(button2);
+    divPokemoni2.append(divDrugi);
 
+    button2.addEventListener("click", () => {
+      button2.textContent = "";
 
+      let divPok2 = document.createElement("div");
+      divPok2.className = "divPok";
 
+      let divPar2 = document.createElement("p");
+      divPar2.className = "divPar";
 
-buttonProtivnik.addEventListener('click',()=>{
+      divPar2.textContent = `Name: ${pokemon.name}. Type: ${pokemon.type}. Skills: ${pokemon.skills}. stats - Attack: ${pokemon.stats.Attack}, Deffence: ${pokemon.stats.Deffence}, Speed: ${pokemon.stats.Speed}`;
 
+      let buttonIzaberi = document.createElement("button");
+      buttonIzaberi.textContent = "Choose me!";
+      buttonIzaberi.className = "izaberi-button";
 
-  pokemoni.forEach(pokemon => {
+      buttonIzaberi.addEventListener("click", () => {
+        let allDivs = document.querySelectorAll(".drugi");
 
+        allDivs.forEach((div) => {
+          div.remove();
+        });
 
-      let divDrugi = document.createElement('div')
-      divDrugi.className = 'drugi'
-  
-      let button2 = document.createElement('button')
-      button2.className = 'buttonDrugi'
-      button2.textContent = `${pokemon.name}`
-  
-      divDrugi.appendChild(button2)
-      divPokemoni2.append(divDrugi)
+        let pokemonImgDiv = document.createElement("div");
+        pokemonImgDiv.className = "opponent-img-div";
+        let choosenPokemon = document.createElement("img");
+        choosenPokemon.setAttribute("src", pokemon.img.src);
+        choosenPokemon.setAttribute("alt", pokemon.name);
+        choosenPokemon.className = "pokemon-img";
 
-      button2.addEventListener('click', () => {
-          button2.textContent = ''
+        pokemonImgDiv.appendChild(choosenPokemon);
+        divPokemoni2.append(pokemonImgDiv);
+      });
 
-          let divPok2 = document.createElement('div')
-          divPok2.className = 'divPok'
+      // buttonIzaberi.addEventListener("click", () => {
+      //   divDrugi.remove();
+      // });
 
-          let divPar2 = document.createElement('p')
-          divPar2.className = 'divPar'
+      buttonIzaberi.addEventListener("click", () => {
+        pokemon.choosen = true;
+        Object.assign(choosenOpponent, { pokemon });
+      });
 
-          divPar2.textContent = `Name: ${pokemon.name}. Type: ${pokemon.type}. Skills: ${pokemon.skills}. stats - Attack: ${pokemon.stats.Attack}, deffence: ${pokemon.stats.deffence}, speed: ${pokemon.stats.speed}`
+      button2.append(divPar2, buttonIzaberi);
+      divDrugi.append(divPok2);
+    });
+  });
+});
 
-          let buttonIzaberi = document.createElement('button')
-          buttonIzaberi.textContent = 'Choose me!'
-          buttonIzaberi.className = 'izaberi-button'
+buttonProtivnik.addEventListener("click", () => {
+  buttonProtivnik.remove();
+});
 
-          buttonIzaberi.addEventListener('click', ()=>{
+fightButton.addEventListener("click", () => {
+  pokemoni.forEach((pokemon) => {
+    if (pokemon.choosen == true) {
+      if (
+        choosenPokemon.pokemon.type == "Fire" &&
+        choosenOpponent.pokemon.type == "Electric"
+      ) {
+        choosenPokemon.pokemon.stats.Attack += 2;
+        choosenPokemon.pokemon.stats.Deffence += 2;
+        choosenOpponent.pokemon.stats.Attack -= 1;
+        choosenOpponent.pokemon.stats.Deffence -= 1;
+        console.log(choosenPokemon.pokemon.stats);
+        console.log(choosenOpponent.pokemon.stats);
+      } else if (
+        choosenPokemon.pokemon.type == "Water" &&
+        choosenOpponent.pokemon.type == "Fire"
+      ) {
+        choosenPokemon.pokemon.stats.Attack += 2;
+        choosenPokemon.pokemon.stats.Deffence += 2;
+        choosenOpponent.pokemon.stats.Attack -= 1;
+        choosenOpponent.pokemon.stats.Deffence -= 1;
+        console.log(choosenPokemon.pokemon.stats);
+        console.log(choosenOpponent.pokemon.stats);
+      } else if (
+        choosenPokemon.pokemon.type == "Electric" &&
+        choosenOpponent.pokemon.type == "Water"
+      ) {
+        choosenPokemon.pokemon.stats.Attack += 2;
+        choosenPokemon.pokemon.stats.Deffence += 2;
+        choosenOpponent.pokemon.stats.Attack -= 1;
+        choosenOpponent.pokemon.stats.Deffence -= 3;
+        console.log(choosenPokemon.pokemon.stats);
+        console.log(choosenOpponent.pokemon.stats);
+      } else if (
+        choosenPokemon.pokemon.type == "Electric" &&
+        choosenOpponent.pokemon.type == "Fire"
+      ) {
+        choosenPokemon.pokemon.stats.Attack -= 1;
+        choosenPokemon.pokemon.stats.Deffence -= 1;
+        choosenOpponent.pokemon.stats.Attack += 2;
+        choosenOpponent.pokemon.stats.Deffence += 2;
+        console.log(choosenPokemon.pokemon.stats);
+        console.log(choosenOpponent.pokemon.stats);
+      } else if (
+        choosenPokemon.pokemon.type == "Fire" &&
+        choosenOpponent.pokemon.type == "Water"
+      ) {
+        choosenPokemon.pokemon.stats.Attack -= 1;
+        choosenPokemon.pokemon.stats.Deffence -= 1;
+        choosenOpponent.pokemon.stats.Attack += 2;
+        choosenOpponent.pokemon.stats.Deffence += 2;
+        console.log(choosenPokemon.pokemon.stats);
+        console.log(choosenOpponent.pokemon.stats);
+      } else if (
+        choosenPokemon.pokemon.type == "Electric" &&
+        choosenOpponent.pokemon.type == "Water"
+      ) {
+        choosenPokemon.pokemon.stats.Attack -= 1;
+        choosenPokemon.pokemon.stats.Deffence -= 1;
+        choosenOpponent.pokemon.stats.Attack += 2;
+        choosenOpponent.pokemon.stats.Deffence += 2;
+        console.log(choosenPokemon.pokemon.stats);
+        console.log(choosenOpponent.pokemon.stats);
+      }
 
+      if (
+        choosenPokemon.pokemon.stats.Attack >
+        choosenOpponent.pokemon.stats.Deffence
+      ) {
+        let winnerDiv = document.querySelector(".winner");
 
-            let pokemonImgDiv = document.createElement('div');
-            pokemonImgDiv.className = 'opponent-img-div';
-            let choosenPokemon = document.createElement('img');
-            choosenPokemon.setAttribute('src', pokemon.img.src);
-            choosenPokemon.setAttribute('alt', pokemon.name);
-            choosenPokemon.className = 'pokemon-img'
+        let winner = document.createElement("div");
+        winner.className = "winner-div";
 
-            pokemonImgDiv.appendChild(choosenPokemon)
-            divPokemoni2.append(pokemonImgDiv);            
-          }); 
+        let winnerText = document.createElement("p");
+        winnerText.className = "winner-text";
+        winnerText.textContent = "Omg! The left Player has won!";
 
-          buttonIzaberi.addEventListener('click', ()=>{
-            divDrugi.remove();
-          });
+        winner.appendChild(winnerText);
+        winnerDiv.append(winner);
 
-          buttonIzaberi.addEventListener('click', ()=>{
-            pokemon.choosen = true;
-          });
-          
+        fightButton.remove();
 
+        return console.log("Omg! The left Player has won!");
+      } else if (
+        choosenPokemon.pokemon.stats.Attack <=
+        choosenOpponent.pokemon.stats.Deffence
+      ) {
+        let winnerDiv = document.querySelector(".winner");
+        let winner = document.createElement("div");
+        winner.className = "winner-div";
 
-          button2.append(divPar2, buttonIzaberi)
-          divDrugi.append(divPok2)
+        let winnerText = document.createElement("p");
+        winnerText.className = "winner-text";
+        winnerText.textContent = "Omg! The right Player has won!";
 
-  })
-  
-      })
+        winner.appendChild(winnerText);
+        winnerDiv.append(winner);
 
+        fightButton.remove();
 
-    
-  
-})
-
-buttonProtivnik.addEventListener('click',()=>{
-  buttonProtivnik.remove()
-})
+        return console.log("Omg! The right Player has won!");
+      }
+    }
+  });
+});
